97d062405c1478bab3683c00fe7791682fedf6a5
taylornguyen1 <43552916+taylornguyen1@users.noreply.github.com>
2021-03-31 21:41:13 -0700

    Update app.py

--- storage/app.py
+++ storage/app.py
@@ -34,9 +34,14 @@ else:
         print("In Dev Environment")
         app_conf_file = "/config/app_conf.yml"
         app_conf_file = "/config/log_conf.yml"
+
 with open(app_conf_file, 'r') as f:
         app_config = yaml.safe_load(f.read())
+
+with open(log_conf_file, 'r') as f:
+        log_config = yaml.safe_load(f.read())
         logging.config.dictConfig(log_config)
+
 logger = logging.getLogger('basicLogger')
 
 logger.info("App Conf File: %s" % app_conf_file)
