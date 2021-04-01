3cd55b3d7d9e3bc168e24a70f28e6cfc5861ba3b
taylornguyen1 <43552916+taylornguyen1@users.noreply.github.com>
2021-03-31 22:24:17 -0700

    app.py

--- receiver/app.py
+++ receiver/app.py
@@ -24,25 +24,26 @@ EVENT_FILE = "events.json"
 #logger = logging.getLogger('basicLogger')
 
 if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
-        print("In Test Environment")
-        app_conf_file = "/config/app_conf.yml"
-        app_conf_file = "/config/log_conf.yml"
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
 else:
-        print("In Dev Environment")
-        app_conf_file = "/config/app_conf.yml"
-        app_conf_file = "/config/log_conf.yml"
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
 
 with open(app_conf_file, 'r') as f:
-        app_config = yaml.safe_load(f.read())
+    app_config = yaml.safe_load(f.read())
 
+# External Logging Configuration
 with open(log_conf_file, 'r') as f:
-        log_config = yaml.safe_load(f.read())
-        logging.config.dictConfig(log_config)
+    log_config = yaml.safe_load(f.read())
+    logging.config.dictConfig(log_config)
 
 logger = logging.getLogger('basicLogger')
 
 logger.info("App Conf File: %s" % app_conf_file)
-logger.info("Log Conf File: %s" % app_conf_file)
+logger.info("Log Conf File: %s" % log_conf_file)
 
 
 def total_views(body):
